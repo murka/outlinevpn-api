edaaa8d7d30d5cdf8ef11670ed83ebe2873f53b0
Danil Shaymurzin <via@mrzn.org>
2022-08-22 20:09:04 +0500

    feat: add new methods for server

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,95 @@
 import https from 'node:https'
 import nodefetch, { RequestInfo, RequestInit, Headers } from 'node-fetch'
-import { User } from "./types"
+import type {
+    User,
+    Server,
+    DataUsageByUser,
+    ServerMetrics
+} from "./types"
 
 const fetch = (url: RequestInfo, init?: RequestInit) => nodefetch(url, { ...init, agent: new https.Agent({ rejectUnauthorized: false }) })
+
 
 export default class OutlineVPN {
     apiUrl: string
     constructor(apiUrl: string) {
         this.apiUrl = apiUrl
+    }
+
+    public async getServer(): Promise<Server> {
+        const response = await fetch(`${this.apiUrl}/server`)
+        const json = await response.json()
+        return json
+    }
+
+    public async renameServer(name: string): Promise<boolean> {
+        const response = await fetch(`${this.apiUrl}/name`, {
+            method: 'PUT',
+            headers: new Headers({ 'Content-Type': 'application/json' }),
+            body: JSON.stringify({ name })
+        })
+
+        return response.ok
+    }
+
+    public async setDefaultDataLimit(bytes: number): Promise<boolean> {
+        const response = await fetch(`${this.apiUrl}/server/access-key-data-limit`, {
+            method: 'PUT',
+            headers: new Headers({ 'Content-Type': 'application/json' }),
+            body: JSON.stringify({ limit: { bytes } })
+        })
+
+        return response.ok
+    }
+
+    public async deleteDefaultDataLimit(): Promise<boolean> {
+        const response = await fetch(`${this.apiUrl}/server/access-key-data-limit`, {
+            method: 'DELETE'
+        })
+
+        return response.ok
+    }
+
+    public async setHostnameForAccessKeys(hostname: string): Promise<boolean> {
+        const response = await fetch(`${this.apiUrl}/server/hostname-for-access-keys`, {
+            method: 'PUT',
+            headers: new Headers({ 'Content-Type': 'application/json' }),
+            body: JSON.stringify({ hostname })
+        })
+
+        return response.ok
+    }
+
+    public async setPortForNewAccessKeys(port: number): Promise<boolean> {
+        const response = await fetch(`${this.apiUrl}/server/port-for-new-access-keys`, {
+            method: 'PUT',
+            headers: new Headers({ 'Content-Type': 'application/json' }),
+            body: JSON.stringify({ port })
+        })
+
+        return response.ok
+    }
+
+    public async getDataUsage(): Promise<DataUsageByUser> {
+        const response = await fetch(`${this.apiUrl}/metrics/transfer`)
+        const json = await response.json()
+        return json
+    }
+
+    public async getShareMetrics(): Promise<ServerMetrics> {
+        const response = await fetch(`${this.apiUrl}/metrics/enabled`)
+        const json = await response.json()
+        return json
+    }
+
+    public async setShareMetrics(metricsEnabled: boolean): Promise<boolean> {
+        const response = await fetch(`${this.apiUrl}/metrics/enabled`, {
+            method: 'PUT',
+            headers: new Headers({ 'Content-Type': 'application/json' }),
+            body: JSON.stringify({ metricsEnabled })
+        })
+
+        return response.ok
     }
 
     public async getUsers(): Promise<User[]> {
--- src/types.ts
+++ src/types.ts
@@ -6,3 +6,25 @@ export interface User {
     method: string,
     accessUrl: string
 }
+
+export interface Server {
+    name: string,
+    serverId: string,
+    metricsEnabled: boolean,
+    createdTimestampMs: number,
+    version: string,
+    portForNewAcccessKeys: number,
+    hostnameForNewAccessKeys: string
+}
+
+export interface DataUsageByUser {
+    // The userId key should be of type AccessKeyId, however that results in the tsc
+    // error TS1023: An index signature parameter type must be 'string' or 'number'.
+    // See https://github.com/Microsoft/TypeScript/issues/2491
+    // TODO: rename this to AccessKeyId in a backwards compatible way.
+    bytesTransferredByUserId: { [userId: string]: number }
+}
+
+export interface ServerMetrics {
+    metricsEnabled: boolean
+}
